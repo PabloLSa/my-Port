c57106eaee70b0107d36ddab8feb69604406873e
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2023-07-14 08:56:18 -0300

    :wrench: Home
    Ajustes UseState ThemeAbout

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -9,7 +9,7 @@ function About() {
   const theme = useContext(ThemeContext);
   const [themeClasses, setThemeClasses] = useState('bg-gradient-to-br from-zinc-900 to-zinc-800 mx-auto py-8 min-h-screen');
   const [themeText, setThemeText] = useState(`bg-gradient-to-br from-blue-500  to-blue-700 text-transparent bg-clip-text`);
-  const [themeAbout, setThemeAbout] = useState(`font-sans font-bold text-xl sm:text-xl text-center text-white`);
+  const [themeAbout, setThemeAbout] = useState(`font-sans font-bold text-xl sm:text-2xl text-center text-white`);
 
   useEffect(() => {
     setThemeClasses(
