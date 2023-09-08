40a9a0c33d9937e2c0f4400bda07ef1698a2461c
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2023-09-08 17:00:39 -0300

    :art: About
     teste framer no about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -20,7 +20,7 @@ function Section({ children } : any) {
         style={{
           transform: isInView ? "none" : "translateX(-200px)",
           opacity: isInView ? 1 : 0,
-          transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 6s"
+          transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 5s"
         }}
       >
         {children}
